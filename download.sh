652ddfc41016c4ac9a7814546588a73b50b7a584
Dennis Porter Jr <dennisporterjr@gmail.com>
2020-04-04 10:14:25 -0400

    removing wp tar now

--- download.sh
+++ download.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
+printf "**********************\n"
+printf "* Download Wordpress *\n"
+printf "**********************\n\n"
+
 # download wordpress
 wget http://wordpress.org/latest.tar.gz
+
+printf "*******************\n"
+printf "* Extract Package *\n"
+printf "*******************\n\n"
 
 # extract wordpress
 tar xfz latest.tar.gz
 
+printf "************\n"
+printf "* Clean Up *\n"
+printf "************\n\n"
 # move to current directory
 mv -v ./wordpress/* .
 
 # remove wordpress folder
 rm -r wordpress
+rm latest.tar.gz
+
+printf "************\n"
+printf "* Clean Up *\n"
+printf "************\n\n"
